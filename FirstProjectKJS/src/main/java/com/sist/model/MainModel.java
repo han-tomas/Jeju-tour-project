c869386a8b83f32afd3d9cd31c96d8f7281bdbb5
caelum4376 <129152864+caelum4376@users.noreply.github.com>
2023-07-04 18:06:43 +0900

    UI수정 및 로그인/로그아웃 기능 추가

---
+++ FirstProjectKJS/src/main/java/com/sist/model/MainModel.java
@@ -0,0 +1,16 @@
+package com.sist.model;
+import java.util.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sist.controller.RequestMapping;
+import com.sist.dao.*;
+import com.sist.vo.*;
+public class MainModel {
+	@RequestMapping("main/main.do")
+	public String main_page(HttpServletRequest request, HttpServletResponse response) {
+		request.setAttribute("main_jsp", "../main/home.jsp");
+		return "../main/main.jsp";
+	}
+}
